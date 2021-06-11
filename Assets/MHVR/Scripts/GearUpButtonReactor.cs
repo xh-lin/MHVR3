d7045eb35dd4aa4daf732b44ed7edda39b76a7c9
Xu Huang Lin <XuHuang.Lin001@umb.edu>
2021-06-11 16:42:37 -0400

    unequip function

--- Assets/MHVR/Scripts/GearUpButtonReactor.cs
+++ Assets/MHVR/Scripts/GearUpButtonReactor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using VRTK.Controllables;
+using VRTK;
 
 public class GearUpButtonReactor : MonoBehaviour
 {
@@ -22,14 +23,40 @@ public class GearUpButtonReactor : MonoBehaviour
     protected virtual void MaxLimitReached(object sender, ControllableEventArgs e)
     {
         if (avatar.equip == null) {
-            GameObject equipInstance = Instantiate(equipPrefab);
-            avatar.equip = equipInstance;
-            equipInstance.transform.SetParent(avatar.backpack.transform);
-            equipInstance.transform.localPosition = Vector3.zero;
+            Equip();
+        } else {
+            Unequip();
+        }
+    }
 
-            if (displayText != null) {
-                displayText.text = "1";
+    private void Equip()
+    {
+        GameObject equipInstance = Instantiate(equipPrefab);
+        avatar.equip = equipInstance;
+        equipInstance.transform.SetParent(avatar.backpack.transform);
+        equipInstance.transform.localPosition = Vector3.zero;
+        equipInstance.transform.localRotation = Quaternion.identity;
+
+        if (displayText != null) {
+            displayText.text = "1";
+        }
+    }
+
+    private void Unequip()
+    {
+        VRTK_SnapDropZone[] snapDropZones = avatar.equip.GetComponentsInChildren<VRTK_SnapDropZone>();
+        foreach (var sdz in snapDropZones) {
+            var dsiObj = sdz.defaultSnappedInteractableObject;
+            if (dsiObj != null) {
+                if (dsiObj.IsGrabbed())
+                    dsiObj.GetGrabbingObject().GetComponent<VRTK_InteractGrab>().ForceRelease();
+                Destroy(dsiObj.gameObject);
             }
+        }
+        Destroy(avatar.equip);
+
+        if (displayText != null) {
+            displayText.text = "0";
         }
     }
 }
